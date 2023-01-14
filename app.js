551559f48d59bc92e4c884e7b5da3b0c1453124a
MIINII <ft.dawn.o@gmail.com>
2023-01-14 16:34:12 +0900

    세팅중

---
+++ app.js
@@ -0,0 +1,37 @@
+// Firebase
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getAuth, onAuthStateChanged, getRedirectResult } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_APIKEY,
+//   authDomain: process.env.REACT_APP_AUTHDOMAIN,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID,
+// };
+
+const firebaseApp = initializeApp({
+  /* config */
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+});
+
+const auth = getAuth(firebaseApp);
+onAuthStateChanged(auth, (user) => {
+  /* check status */
+});
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+// dotenv
+import dotenv from 'dotenv';
--- server/app.js
+++
@@ -1,7 +0,0 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
-
-const firebase = require('firebase');
-
-// env
-require('dotenv').config();
